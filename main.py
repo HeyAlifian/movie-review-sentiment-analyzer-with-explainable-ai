e835681c4d0aeb2a8f82dbc9fe8e030ea9d5d5e1
Alifian <Alifianrasha01@gmail.com>
2025-09-07 21:22:05 +0700

    Added all the files

---
+++ main.py
@@ -0,0 +1,41 @@
+import re
+import joblib
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+# Load 'em models.
+model      = joblib.load(r"Models\sentiment_model.pkl")
+vectorizer = joblib.load(r"Models\tfidf_vectorizer.pkl")
+
+stop_words = set(stopwords.words("english"))
+tokenizer = RegexpTokenizer(r"[A-Za-z']+")
+
+def clean_text(text):
+
+    text = re.sub(r'<.*?>', ' ', text)
+    
+    tokens = tokenizer.tokenize(text.lower())
+    
+    tokens = [t for t in tokens if t not in stop_words]
+
+    return " ".join(tokens)
+
+def predict_sentiment(review):
+
+    cleaned = clean_text(review)
+    
+    X_vectorized = vectorizer.transform([cleaned])
+    
+    prediction = model.predict(X_vectorized)[0]
+    probability_percentage = model.predict_proba(X_vectorized)[0]
+    
+    sentiment = "Positive :)" if prediction == 1 else "Negative :("
+
+    return sentiment, probability_percentage
+
+# Main Code
+review = "The movie was great and incredible! This is worth the wait."
+sentiment, probability_percentage = predict_sentiment(review)
+
+print("Sentiment:", sentiment)
+print("Percentage:", probability_percentage)
---
+++ model_train.py
@@ -0,0 +1,91 @@
+# from sklearn.datasets import fetch_20newsgroups
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from lime.lime_text import LimeTextExplainer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+import joblib
+import pandas
+import re
+import nltk
+
+nltk.download("stopwords")
+
+class RawDProcessor:
+    def __init__(self, dataset_path: str, drop_na: bool = True, separator: any = ",") -> None:
+
+        assert isinstance(dataset_path, str), f"Invalid type of 'dataset_path', expected str. Found {type(dataset_path)}"
+        assert isinstance(drop_na, bool), f"Invalid type of 'drop_na', expected bool. Found {type(dataset_path)}"
+
+        self.data           = pandas.read_csv(dataset_path, sep=separator)
+        self.isdrop_na      = drop_na
+        self.stop_words     = set(stopwords.words('english'))
+        self.tokenizer      = RegexpTokenizer(r"[A-Za-z']+")
+        self.data[
+            "clean_review"
+            ]               = self.data["review"].apply(self.CleanData)
+        self.Head()
+        self.Return()
+    
+    def CleanData(self, data):
+
+        data                = re.sub(r'<.*?>', ' ', data)
+        tokens              = self.tokenizer.tokenize(data.lower())
+        tokens              = [t for t in tokens if t not in self.stop_words]
+
+        return " ".join(tokens)
+
+    def Head(self):
+        print(self.data.head())
+
+    def Return(self):
+        return self.data
+    
+def SaveModel(model, file_name: str, path: str = r"Models") -> None:
+    if model != "":
+        try:
+            joblib.dump(model, f"{path}/{file_name}")
+            print(f"SUCCESSFULLY SAVED DATA TO: {path}/{file_name}")
+        except Exception as err:
+            print(f"[ERROR]:", err)
+    else:
+        print(f"NO MODELS INPUTTED.")
+
+
+# Main Code
+dataset_path    = r"Datasets\imdb-datasets.csv"
+
+dataset         = RawDProcessor(dataset_path=dataset_path, drop_na=True, separator=',').Return()
+
+X               = dataset["clean_review"]
+y               = dataset["sentiment"].map({"negative": 0, "positive": 1})
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.2, random_state = 42, stratify = y
+)
+
+vectorizer      = TfidfVectorizer(max_features = 10_000, ngram_range=(1,2))
+X_train_tfidf   = vectorizer.fit_transform(X_train)
+X_test_tfidf    = vectorizer.transform(X_test)
+
+model           = LogisticRegression(max_iter = 200)
+model.fit(X_train_tfidf, y_train)
+
+y_prediction    = model.predict(X_test_tfidf)
+
+print("ACCURACY:", accuracy_score(y_test, y_prediction))
+print("\nCLASSIFICATION REPORT:\n---------------------------------------\n",classification_report(y_test, y_prediction))
+
+X_all           = dataset['clean_review']
+y_all           = dataset["sentiment"].map({'negative':0, 'positive':1})
+
+print("ALL DATA TRAIN")
+X_all_tfidf     = vectorizer.fit_transform(X_all)
+final_model     = LogisticRegression(max_iter=200)
+final_model.fit(X_all_tfidf, y_all)
+
+SaveModel(final_model, "sentiment_model.pkl")
+SaveModel(vectorizer, "tfidf_vectorizer.pkl")
